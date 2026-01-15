38c0b777509c471503148cf5ebfa3c162e122992
mozangies-prog <mozangies@gmail.com>
2026-01-15 22:44:22 +0500

    Create App.jsx file with initial React component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{padding: 24}}>
+      <h1 style={{fontSize: 24, fontWeight: 700}}>SyncNote Chat</h1>
+      <p>If you see this, the app bundle loaded successfully.</p>
+    </div>
+  )
+}
